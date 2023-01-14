01868803b6dbd8682aa7b1e54b2e7d600ab04cd9
Lucas Behne <daniel.behne@mrjaffesclass.com>
2023-01-13 22:01:05 -0800

    Arrays for suit and tens

--- blackjack/Cards.java
+++ blackjack/Cards.java
@@ -1,16 +1,19 @@
+import java.util.Random;
+
 public class Cards {
     private int value;
     private String suit;
-    String spades;
-    String clubs;
-    String hearts;
-    String diamonds;
+    private String[] Suit = {"Spades", "Clubs", "Hearts", "Diamonds"};
+    private String[] Tens = {"10", "Jack", "Queen", "King"};
     public void getCard() {
         int value = (int) (Math.random() * 10) + 1;
         if (value == 1) {
             System.out.println("Ace");
         } else {
             System.out.println(value);
+        }
+        if (value == 10) {
+            
         }
     }
 }
