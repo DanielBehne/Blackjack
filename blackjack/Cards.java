d1d35694462c05b9df5646ac10953553a271625e
Daniel Behne <daniel.behne@mrjaffesclass.com>
2023-01-17 14:22:53 -0800

    Finished random card generator

--- blackjack/Cards.java
+++ blackjack/Cards.java
@@ -2,18 +2,19 @@ import java.util.Random;
 
 public class Cards {
     private int value;
-    private String suit;
-    private String[] Suit = {"Spades", "Clubs", "Hearts", "Diamonds"};
-    private String[] Tens = {"10", "Jack", "Queen", "King"};
+    private String[] suit = {"Spades", "Clubs", "Hearts", "Diamonds"};
+    private String[] tens = {"10", "Jack", "Queen", "King"};
     public void getCard() {
         int value = (int) (Math.random() * 10) + 1;
         if (value == 1) {
-            System.out.println("Ace");
+            Random random1 = new Random();
+            System.out.println(("Ace") + (" of ") + suit[random1.nextInt(suit.length)]);
+        } else if (value == 10) {
+            Random random3 = new Random();
+            System.out.println(tens[random3.nextInt(tens.length)] + (" of ") + suit[random3.nextInt(suit.length)]);
         } else {
-            System.out.println(value);
-        }
-        if (value == 10) {
-            
-        }
+            Random random2 = new Random();
+            System.out.println((value) + (" of ") + suit[random2.nextInt(suit.length)]);
+        }  
     }
 }
